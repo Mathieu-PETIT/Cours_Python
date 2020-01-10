2ce53a99dfe01c4ce8506979a2a1ff5c1a57b5e0
Mathieu-PETIT <57092402+Mathieu-PETIT@users.noreply.github.com>
2020-01-10 16:34:54 +0100

    Update Fonctions.py

--- Fonctions.py
+++ Fonctions.py
@@ -1,4 +1,6 @@
-def compter_nb_elements():
+from datetime import datetime
+
+def compter_nb_elements(csvfichier):
     # On part de -1 pour ne pas compter la première qui sert pour les catégories
     elements = -1
     # On compte le nombre de lignes présentes dans le fichier csv
@@ -6,5 +8,63 @@ def compter_nb_elements():
         elements+=1
     return elements
 
-print(compter_nb_elements())
-
+def verif_date(d):
+    if len(d) == 10:
+        vd=datetime.now()
+        dt=vd.strftime('%Y-%m-%d')
+        print(dt)
+        part=d[0]+d[1]+d[2]+d[3]
+        partd=dt[0]+dt[1]+dt[2]+dt[3]
+        if part.isdigit()==True and int(part)<int(partd):
+            if d[4]=="-":
+                part=d[5]+d[6]
+                if int(part)<=12:
+                    if part.isdigit()==True:
+                        if d[7]=="-":
+                            part=d[8]+d[9]
+                            if part.isdigit()==True:
+                                print(d+" est bien une date ")
+                                return True
+                            print("Format date incorrecte")
+                            return False
+                        print("Format date incorrect")
+                        return False
+                    print("Format date incorrect")
+                    return False
+                print("Format date incorrect")
+                return False
+            print("Format date incorrect")
+            return False
+        elif part.isdigit()==True and int(part)==int(partd):
+            if d[4] == "-":
+                part = d[5] + d[6]
+                partd=d[5] + d[6]
+                if int(part)<partd and part.isdigit()==True:
+                    if d[7] == "-":
+                        part = d[8] + d[9]
+                        if part.isdigit() == True:
+                            print("Yes")
+                elif int(part)==partd and part.isdigit()==True:
+                    if d[7] == "-":
+                        part = d[8] + d[9]
+                        partd=d[8] + d[9]
+                        if part.isdigit()==True and int(part)<=partd:
+                            return True
+                        else:
+                            print("Format date incorrect")
+                            return False
+                    else:
+                        print("Format date incorrect")
+                        return False
+                else:
+                    print("Format date incorrect")
+                    return False
+            else:
+                print("Format date incorrect")
+                return False
+        else:
+            print("Format date incorrect")
+            return False
+    else:
+        print("Format date incorrect")
+        return False
